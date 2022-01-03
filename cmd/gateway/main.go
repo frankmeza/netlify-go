34965496a21418a1fe48126e6469b15e7d68f500
Frank Meza <frankmezapdx@gmail.com>
2022-01-02 19:05:17 -0800

    throw it all at the wall and see what sticks

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	// "embed"
 	"flag"
 	"fmt"
 	"log"
@@ -13,17 +14,28 @@ import (
 func main() {
 	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
 	flag.Parse()
+
 	listener := gateway.ListenAndServe
 	portStr := ""
+
 	if *port != -1 {
 		portStr = fmt.Sprintf(":%d", *port)
 		listener = http.ListenAndServe
+
+		// distFS, err := fs.Sub(nextFS, "nextjs/dist")
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// // The static Next.js app will be served under `/`.
+		// http.Handle("/", http.FileServer(http.FS(distFS)))
 		http.Handle("/", http.FileServer(http.Dir("./public")))
 	}
 
 	http.Handle("/api/feed", feed2json.Handler(
 		feed2json.StaticURLInjector("https://news.ycombinator.com/rss"),
-		nil, nil, nil, cacheControlMiddleware))
+		nil, nil, nil, cacheControlMiddleware,
+	))
+
 	log.Fatal(listener(portStr, nil))
 }
 
