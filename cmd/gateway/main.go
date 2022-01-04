0b3c3341c8c3b2b16d0aad359d11ab03af043b81
Frank Meza <frankmezapdx@gmail.com>
2022-01-03 18:38:32 -0800

    still messing around

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -19,29 +19,40 @@ import (
 var nextFS embed.FS
 
 func main() {
-	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
-	flag.Parse()
-
-	if *port == -1 {
-		log.Fatal(gateway.ListenAndServe("", nil))
-		return
-	}
-
 	distFS, err := fs.Sub(nextFS, "nextjs/dist")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// The static Next.js app will be served under `/`.
-	http.Handle("/", http.FileServer(http.FS(distFS)))
+	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
+	flag.Parse()
+
+	// if *port == -1 {
+	// 	log.Fatal(gateway.ListenAndServe("", nil))
+	// 	return
+	// }
+	listener := gateway.ListenAndServe
+	portStr := "n/a"
+	if *port != -1 {
+		portStr = fmt.Sprintf(":%d", *port)
+		listener = http.ListenAndServe
+		http.Handle("/", http.FileServer(http.Dir("./public")))
+	}
+
+	http.HandleFunc("/api/chuck", handleChuckJoke)
 	// http.Handle("/", http.FileServer(http.Dir("./public")))
 
 	http.Handle("/api/feed", feed2json.Handler(
-		feed2json.StaticURLInjector("https://news.ycombinator.com/rss"),
-		nil, nil, nil, cacheControlMiddleware,
-	))
+		feed2json.StaticURLInjector("https://news.ycombinator.com/rss"), nil, nil, nil))
+	http.Handle("/", http.FileServer(http.FS(distFS)))
+	log.Fatal(listener(portStr, nil))
 
-	http.HandleFunc("/api/chuck", handleChuckJoke)
+	// http.Handle("/api/feed", feed2json.Handler(
+	// 	feed2json.StaticURLInjector("https://news.ycombinator.com/rss"),
+	// 	nil, nil, nil, cacheControlMiddleware,
+	// ))
+
+	// The static Next.js app will be served under `/`.
 }
 
 func handleChuckJoke(w http.ResponseWriter, r *http.Request) {
