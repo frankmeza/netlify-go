62867750a9f047b7ef574d4d00765c2e2dda6f45
Frank Meza <frankmezapdx@gmail.com>
2022-01-03 19:19:59 -0800

    full circle

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -35,11 +35,11 @@ func main() {
 		feed2json.StaticURLInjector("https://news.ycombinator.com/rss"), nil, nil, nil))
 
 	if *port == -1 {
-		log.Fatal(gateway.ListenAndServe("https://jovial-swirles-5cc968.netlify.app", nil))
+		log.Fatal(gateway.ListenAndServe("", nil))
 		return
 	}
 
-	log.Fatal(gateway.ListenAndServe("/", nil))
+	log.Fatal(gateway.ListenAndServe("", nil))
 
 	// http.Handle("/", http.FileServer(http.Dir("./public")))
 }
