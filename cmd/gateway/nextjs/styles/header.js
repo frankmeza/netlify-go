de04bad5878bc1d893cd9eb7639ac1bf6909fb3c
Frank Meza <frankmezapdx@gmail.com>
2022-01-11 21:37:56 -0800

    match indent style of other files

--- cmd/gateway/nextjs/styles/header.js
+++ cmd/gateway/nextjs/styles/header.js
@@ -4,85 +4,85 @@
 import css from "styled-jsx/css";
 
 export default css.header`
-.styled_header {
-    padding-top: var(--size-300);
-}
+    .styled_header {
+        padding-top: var(--size-300);
+    }
 
-.header_wrapper {
-    display: flex;
-    justify-content: space-between;
-    margin-left: auto;
-    margin-right: auto;
-    max-width: 1300px;
-    padding-left: var(--size-700);
-    padding-right: var(--size-700);
-    height: 100%;
-}
-
-@media screen and (max-width: 1000px) {
     .header_wrapper {
-        padding-left: var(--size-400);
-        padding-right: var(--size-400);
+        display: flex;
+        justify-content: space-between;
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 1300px;
+        padding-left: var(--size-700);
+        padding-right: var(--size-700);
+        height: 100%;
     }
-}
 
-.header_title {
-    margin-right: auto;
-    max-width: 1300px;
-    padding-left: var(--size-700);
-    padding-right: var(--size-700);
-    height: 100%;
-}
+    @media screen and (max-width: 1000px) {
+        .header_wrapper {
+            padding-left: var(--size-400);
+            padding-right: var(--size-400);
+        }
+    }
 
-.header_title a {
-    text-decoration: none;
-    background-color: none;;
-}
-
-@media screen and (max-width: 1000px) {
     .header_title {
-        padding-left: var(--size-400);
-        padding-right: var(--size-400);
+        margin-right: auto;
+        max-width: 1300px;
+        padding-left: var(--size-700);
+        padding-right: var(--size-700);
+        height: 100%;
     }
-}
 
-.styled_nav {
-    position: static;
-    padding: 0;
-    background: transparent;
-    backdrop-filter: unset;
-}
+    .header_title a {
+        text-decoration: none;
+        background-color: none;;
+    }
 
-.styled_nav_list {
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    padding: 0;
-    list-style-type: none;
-}
+    @media screen and (max-width: 1000px) {
+        .header_title {
+            padding-left: var(--size-400);
+            padding-right: var(--size-400);
+        }
+    }
 
-.styled_nav_list_item:not(:last-of-type) {
-    margin-right: 2rem;
-}
+    .styled_nav {
+        position: static;
+        padding: 0;
+        background: transparent;
+        backdrop-filter: unset;
+    }
 
-@media screen and (max-width: 700px) {
+    .styled_nav_list {
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        padding: 0;
+        list-style-type: none;
+    }
+
     .styled_nav_list_item:not(:last-of-type) {
-        margin-right: 1rem;
+        margin-right: 2rem;
     }
-}
 
-.styled_nav_list_item a {
-    color: inherit;
-    text-transform: uppercase;
-    font-size: var(--size-300);
-    text-decoration: none;
-    letter-spacing: 0.1rem;
-}
+    @media screen and (max-width: 700px) {
+        .styled_nav_list_item:not(:last-of-type) {
+            margin-right: 1rem;
+        }
+    }
 
-@media screen and (max-width: 700px) {
     .styled_nav_list_item a {
-        font-size: 0.7rem;
+        color: inherit;
+        text-transform: uppercase;
+        font-size: var(--size-300);
+        text-decoration: none;
+        letter-spacing: 0.1rem;
     }
-}
+
+    @media screen and (max-width: 700px) {
+        .styled_nav_list_item a {
+            font-size: 0.7rem;
+        }
+    }
 `;
