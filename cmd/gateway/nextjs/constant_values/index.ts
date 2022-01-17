dc301d297854d1b7e16a8136eaf42eddc3e9545c
Frank Meza <frankmezapdx@gmail.com>
2022-01-17 00:01:17 -0800

    other edds and onds

---
+++ cmd/gateway/nextjs/constant_values/index.ts
@@ -0,0 +1,23 @@
+export const STRINGS = {
+    ABOUT: "About",
+    CANCEL: "Cancel",
+    CONTACT: "Contact",
+    DECREMENT: "decrement",
+    DOCUMENTATION: "Documentation",
+    EMAIL_ADDRESS: "Email Address",
+    HOME: "Home",
+    INCREMENT: "increment",
+    JOBS: "Jobs",
+    LOGIN: "Log in",
+    MORE: "More",
+    PASSWORD: "Password",
+    REPORT_ISSUE: "Report an issue",
+    SUBMIT: "Submit",
+    SIGN_UP: "Sign up",
+    YOUR_WEBSITE: "YOUR WEBSITE",
+};
+
+export const LINK_HREF = {
+    HOME: "/",
+    LOGIN: "/login",
+};
