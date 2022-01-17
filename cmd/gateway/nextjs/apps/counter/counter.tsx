fd3aacfe68d0ea8f5177506416972155b1866b86
Frank Meza <frankmezapdx@gmail.com>
2022-01-17 01:15:14 -0800

    still trying stuff

--- cmd/gateway/nextjs/apps/counter/counter.tsx
+++ cmd/gateway/nextjs/apps/counter/counter.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { RootState } from "../../../store";
-import { decrement, increment } from "../../../store/slices/counter";
+import { RootState } from "../../store";
+import { decrement, increment } from "../../store/slices/counter";
 
-import { STRINGS } from "../../../constant_values";
+import { STRINGS } from "../../constant_values";
 import classNames from "classnames";
 import useSWR from "swr";
-import { fetcher } from "../../../utils";
+import { fetcher } from "../../utils";
 
 const { DECREMENT, INCREMENT } = STRINGS;
 
