0103a202057a0317a6c1432b505ca0d89c0df557
Frank Meza <frankmezapdx@gmail.com>
2022-03-19 21:30:22 -0700

    tighten up post excerpt ui

--- cmd/gateway/nextjs/components/posts.tsx
+++ cmd/gateway/nextjs/components/posts.tsx
@@ -4,12 +4,11 @@ import * as AboutProps from "../pages/blog_content/about.mdx";
 
 import postExcerptStyles from "../styles/posts.js";
 
-// need to make excerpt component for blog summary on index pages
-
 interface MdxProps {
     publishDate: string;
     summary: string;
     tags: string[];
+    timeToRead: string;
     title: string;
 }
 
@@ -19,21 +18,40 @@ interface MdxPropsMap {
 
 const mdxProps: MdxPropsMap = {
     about: {
-        publishDate: "", // todo
+        publishDate: "August 24, 2021", // todo
         summary: AboutProps["summary"],
-        tags: [""], // todo
+        tags: AboutProps["tags"],
+        timeToRead: AboutProps["timeToRead"] || "1 min",
         title: AboutProps["title"],
     },
 };
 
-const posts = () => {
+const displayPostExcerpts = () => {
     return Object.values(mdxProps).map((mdxProp) => {
         return (
             <li key={mdxProp.title}>
                 <style jsx>{postExcerptStyles}</style>
-                <p>{mdxProp.tags}</p>
+                <div>
+                    {mdxProp.tags.map((tag) => {
+                        return (
+                            <a
+                                className="post-excerpt-tag"
+                                href="#"
+                                onClick={() =>
+                                    alert("add tags page and link from here")
+                                }
+                            >
+                                {tag}
+                            </a>
+                        );
+                    })}
+                </div>
                 <h3>{mdxProp.title}</h3>
                 <p>{mdxProp.summary}</p>
+                <div className="post-excerpt-bottom-row">
+                    <span>{mdxProp.publishDate}</span>
+                    <span>{mdxProp.timeToRead}</span>
+                </div>
             </li>
         );
     });
@@ -44,7 +62,7 @@ const Posts = () => {
         <React.Fragment>
             <style jsx>{postExcerptStyles}</style>
 
-            <ul>{posts()}</ul>
+            <ul>{displayPostExcerpts()}</ul>
         </React.Fragment>
     );
 };
