78f0bf805aa80c577ab2790a963e102a336181f6
Frank Meza <frankmezapdx@gmail.com>
2022-03-19 21:29:50 -0700

    make alpha order

--- cmd/gateway/nextjs/styles/posts.js
+++ cmd/gateway/nextjs/styles/posts.js
@@ -2,22 +2,53 @@ import css from "styled-jsx/css";
 
 export default css.postExcerptStyles`
 ul {
-    padding: 0px;
-    list-style: none;
     display: grid;
-    justify-items: center;
     grid-gap: var(--size-600);
     grid-template-columns: repeat(auto-fit, minmax(35ch, 1fr));
+    justify-items: center;
+    list-style: none;
+    margin: 0 10rem;
+    padding: 0px;
+}
+
+@media screen and (max-width: 500px) {
+    & {
+        display: block;
+    }
 }
 
 li {
-    position: relative;
+    backdrop-filter: blur(10px);
+    background-color: rgba(255, 255, 255, 0.3);
+    border: 1px solid rgba(255, 255, 255, 0.5);
+    border-radius: 8px;
     display: flex;
     flex-direction: column;
     padding: 1.5rem;
-    border: 1px solid rgba(255, 255, 255, 0.5);
-    background-color: rgba(255, 255, 255, 0.3);
-    backdrop-filter: blur(10px);
-    border-radius: 8px;
+    position: relative;
+}
+
+.post-excerpt-tag {
+    background-color: rgba(255, 255, 255, 0.7);
+    border: 1px solid rgb(255, 255, 255);
+    border-radius: 4px;
+    box-sizing: border-box;
+    color: inherit;
+    font-size: var(--size-300);
+    margin-right: 0.6rem;
+    margin-bottom: 0.6rem;
+    padding: 0.2rem 0.6rem;
+    position: relative;
+    text-decoration: none;
+    text-transform: uppercase;
+    z-index: 2;
+}
+
+.post-excerpt-bottom-row {
+    margin-top: 2rem;
+    display: flex;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+}
 }
 `;
